8c83c499c4f7122a578a4b4abb91fe0558a1ec43
JavieW <48198663+JavieW@users.noreply.github.com>
2020-01-30 21:23:10 -0500

    Update interceptor.c

--- interceptor.c
+++ interceptor.c
@@ -378,15 +378,18 @@ static int init_function(void) {
 
 	// initializations to table 
 	spin_lock(&pidlist_lock);
-	for(int s = 1; s < NR_syscalls; s++) {
+	int s;
+	for(s = 1; s < NR_syscalls; s++) {
 		mytable my_table = table[s];
-		my_table.f = NULL; //??? syntax
+		my_table.f = NULL;
 		my_table.intercepted = 0;
 		my_table.monitored = 0;
 		my_table.listcount = 0;
 		// initialize my_list with dummy head
 		struct pid_list *ple=(struct pid_list*)kmalloc(sizeof(struct pid_list), GFP_KERNEL);
-		INIT_LIST_HEAD (&ple->list);
+		if (!ple)
+			return -ENOMEM;
+		INIT_LIST_HEAD(&ple->list);	
 		ple->pid = -1;
 		my_table.my_list = ple->list;
 	}
