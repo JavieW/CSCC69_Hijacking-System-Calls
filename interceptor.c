b693a31c2fcc744dc7e0311b971fa90f5f1e5d9b
Javie Wang <javiewang@Javies-MacBook-Pro.local>
2020-01-31 01:30:23 -0500

    unchecked interceptor

--- interceptor.c
+++ interceptor.c
@@ -279,9 +279,13 @@ void my_exit_group(int status)
  */
 asmlinkage long interceptor(struct pt_regs reg) {
 
+	// check if is monitored, later....
 
+	// log system call parameters
+	log_message(current->pid, reg.ax, reg.bx, reg.cx, reg.dx, reg.si, reg.di, reg.bp);
 
-
+	// call original system call
+	table[reg.ax](reg);
 
 	return 0; // Just a placeholder, so it compiles with no warnings!
 }
@@ -337,10 +341,62 @@ asmlinkage long interceptor(struct pt_regs reg) {
  */
 asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 	
+	// check validation of arguments and root user
+	if (syscall < 0 || syscall > NR_syscalls || syscall == MY_CUSTOM_SYSCALL)
+		return -EINVAL;
+	if (pid != 0 && pid_task(find_vpid(pid1), PIDTYPE_PID) == NULL)
+		return -EINVAL;
+	root = !current_uid();
+
+	// handling four kind of cmds
+	if (cmd == REQUEST_SYSCALL_INTERCEPT) {
+		// only root user can perform this cmd
+		if (!root) 
+			return -EPERM;
+		// intercepting an intercepted cmd
+		if (table[syscall].intercepted == 1)
+			return -EBUSY;
+
+		spin_lock(&calltable_lock);
+		spin_lock(&pidlist_lock);
+		// store original system call
+		table[syscall].f = sys_call_table[syscall];
+		// set status to be intercepted
+		table[syscall].intercepted = 1;
+		spin_unlock(&pidlist_lock);
+
+		set_addr_rw((unsigned long)sys_call_table);
+		// replace with generic interceptor
+		sys_call_table[syscall] = interceptor;
+		set_addr_ro((unsigned long)sys_call_table);
+		spin_unlock(&calltable_lock);
+	} else if (cmd == REQUEST_SYSCALL_RELEASE) {
+		// only root user can perform this cmd
+		if (!root)
+			return -EPERM;
+		// de-intercepting a non-intercepted cmd
+		if (table[syscall].intercepted == 0)
+		 	return -EINVAL;
+		
+		spin_lock(&pidlist_lock);
+		spin_lock(&calltable_lock);
+		// restore orignal system call
+		set_addr_rw((unsigned long)sys_call_table);
+		sys_call_table[syscall] = table[syscall].f;
+		set_addr_ro((unsigned long)sys_call_table);
+		// set status to be non-intercepted
+		spin_unlock(&calltable_lock);
+		spin_unlock(&pidlist_lock);
 
 
-
-
+	// } else if (cmd == REQUEST_START_MONITORING) {
+	// 	if (pid==0 && !root)
+	// 		return -EPERM;
+	// } else if (cmd == REQUEST_STOP_MONITORING) {
+	// 	//
+	} else {
+		return -EINVAL;
+	}
 	return 0;
 }
 
@@ -376,9 +432,10 @@ static int init_function(void) {
 	set_addr_rw((unsigned long)sys_call_table);
 	sys_call_table[MY_CUSTOM_SYSCALL] = my_syscall;
 	sys_call_table[__NR_exit_group] = my_exit_group;
+	set_addr_ro((unsigned long)sys_call_table);
 	spin_unlock(&calltable_lock);
 
-	// initializations to table 
+	// initializations of table 
 	spin_lock(&pidlist_lock);
 	for(s = 1; s < NR_syscalls; s++) {
 		// initialize my_list with dummy head
@@ -412,10 +469,13 @@ static int init_function(void) {
 static void exit_function(void)
 {        
 
-
-
-
-
+	// restore to original syscall
+	spin_lock(&calltable_lock);
+	set_addr_rw((unsigned long)sys_call_table);
+	sys_call_table[MY_CUSTOM_SYSCALL] = orig_custom_syscall;
+	sys_call_table[__NR_exit_group] = orig_exit_group;
+	set_addr_ro((unsigned long)sys_call_table);
+	spin_unlock(&calltable_lock);
 
 }
 
