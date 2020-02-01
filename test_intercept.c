a0fc2a8e8e07d839417bd8325d624322a1c4adfb
Miamoo930 <52798570+Miamoo930@users.noreply.github.com>
2020-02-01 01:44:55 -0500

    Update test_intercept.c

--- test_intercept.c
+++ test_intercept.c
@@ -118,15 +118,13 @@ int main(int argc, char **argv) {
 
 	if (argc>1 && strcmp(argv[1], "nonroot") == 0)
 		return do_nonroot(atoi(argv[2]));
-	
-	printf("It is working \n");
 
 	test("insmod interceptor.ko %s", "", system("insmod interceptor.ko") == 0);
 
 	test("bad MY_CUSTOM_SYSCALL args%s", "",  vsyscall_arg(MY_CUSTOM_SYSCALL, 3, 100, 0, 0) == -EINVAL);
 	
 
-/*
+
 	test("insmod interceptor.ko %s", "", system("insmod interceptor.ko") == 0);
 	test("bad MY_CUSTOM_SYSCALL args%s", "",  vsyscall_arg(MY_CUSTOM_SYSCALL, 3, 100, 0, 0) == -EINVAL);
 	
@@ -139,15 +137,15 @@ int main(int argc, char **argv) {
 	do_release(__NR_exit, 0);
 
 	test_syscall(SYS_open);
-*/
+
 
 	/* The above line of code tests SYS_open.
 	   Feel free to add more tests here for other system calls, 
 	   once you get everything to work; check Linux documentation
 	   for other syscall number definitions.  */
-/*
+
 	test("rmmod interceptor.ko %s", "", system("rmmod interceptor") == 0);
-*/
+
 	return 0;
 }
 
